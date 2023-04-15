506bd6711f1a6fce0cc6f9cf3ae90b2d42fad55b
Antonio arieiro <antonio.arieiro@elofy.com.br>
2023-04-15 20:38:11 -0300

    feat: task1

---
+++ task1.js
@@ -0,0 +1,37 @@
+/*
+1) Observe o trecho de código abaixo:
+
+int INDICE = 13, SOMA = 0, K = 0;
+
+enquanto K < INDICE faça
+
+{
+
+K = K + 1;
+
+SOMA = SOMA + K;
+
+}
+
+imprimir(SOMA);
+
+
+
+Ao final do processamento, qual será o valor da variável SOMA?
+   Resposta:
+O valor da variável SOMA será a soma dos números de 1 a 13, ou seja:
+
+1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 = 91
+
+Portanto, ao final do processamento, o valor da variável SOMA será 91.R
+*/
+let INDICE = 13;
+let SOMA = 0;
+let K = 0;
+
+while (K < INDICE) {
+  K = K + 1;
+  SOMA = SOMA + K;
+}
+
+console.log(SOMA);
